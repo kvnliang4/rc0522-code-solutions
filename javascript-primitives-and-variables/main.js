98fc7b42db1621d60fbf2856f8ca2b7cee992f25
Kevin Liang <kevinliang415@yahoo.com>
2022-05-16 17:35:34 +0000

    compelted javascript primitives and variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,23 @@
+var fullName = 'Kevin Liang';
+
+const isCool = true;
+
+var totalPets = 0;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+
+console.log('Value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
