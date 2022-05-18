2cbda290b1fb72651a7654c0f5bf75eb69fb064d
Kevin Liang <kevinliang415@yahoo.com>
2022-05-18 20:12:42 +0000

    completed javascript-if exercise

---
+++ javascript-if/main.js
@@ -0,0 +1,79 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function startsWithJ(string) {
+  if (string[0] === 'j') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrink(person) {
+  if (person.age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrive(person) {
+  if (person.age >= 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isOldEnoughToDrinkAndDrive(person) {
+  if (person.age >= 21 && person.age >= 16) {
+    return false;
+  } else {
+    return false;
+  }
+}
+
+function categorizeAcidity(ph) {
+  if (ph < 7) {
+    return 'acid';
+  } else if (ph > 7 && ph < 14) {
+    return 'base';
+  } else if (ph === 7) {
+    return 'neutral';
+  } else {
+    return 'invalid ph level';
+  }
+}
+
+function introduceWarnerBro(name) {
+  if (name === 'yakko' || name === 'wakko') {
+    return "We're the warner brothers!";
+  } else if (name === 'dot') {
+    return "I'm cute~";
+  } else {
+    return 'Goodnight everybody!';
+  }
+}
