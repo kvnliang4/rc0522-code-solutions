e95255cdd340f97c34c46acf0703d794ecd46b33
Kevin Liang <kevinliang415@yahoo.com>
2022-05-20 00:30:31 +0000

    completed object maniuplation 1

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,8 @@
+/* exported getValue */
+
+/* return object[key] to get the value of the property */
+
+function getValue(object, key) {
+  var value = object[key];
+  return value;
+}
---
+++ object-manipulation-1/src/get-values.js
@@ -0,0 +1,9 @@
+/* exported getValues */
+
+function getValues(object) {
+  var values = [];
+  for (var i in object) {
+    values.push(object[i]);
+  }
+  return values;
+}
