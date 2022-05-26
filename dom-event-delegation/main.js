8c526dc04a5c146563187df1eecad3f8ce9d7bb8
Kevin Liang <kevinliang415@yahoo.com>
2022-05-26 18:37:37 +0000

    completed dom-event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,12 @@
+var taskList = document.querySelector('.task-list');
+
+taskList.addEventListener('click', click);
+
+function click(event) {
+  console.log('event.target: ' + event.target);
+  console.log('event.target.tagName: ' + event.target.tagName);
+  if (event.target.tagName === 'BUTTON') {
+    console.log('closest .task-list-item', event.target.closest('.task-list-item'));
+    event.target.closest('.task-list-item').remove();
+  }
+}
