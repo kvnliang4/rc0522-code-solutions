d73c604d47342b92ec981f36a7da7526460daade
Kevin Liang <kevinliang415@yahoo.com>
2022-06-08 18:08:14 +0000

    updated

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -2,13 +2,13 @@ var countdownMessage = document.querySelector('.countdown-display');
 
 var count = 4;
 
+var interval = setInterval(countdown, 1000);
+
 function countdown() {
   if (count > 0) {
     countdownMessage.textContent = count--;
   } else {
-    clearInterval(1);
+    clearInterval(interval);
     countdownMessage.textContent = '~Earth Beeeelooowww Us~';
   }
 }
-
-setInterval(countdown, 1000);
