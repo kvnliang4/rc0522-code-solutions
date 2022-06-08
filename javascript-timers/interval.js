0425f65c8a2fabae8af1250bf38cc9dab1e1d37c
Kevin Liang <kevinliang415@yahoo.com>
2022-06-08 17:34:49 +0000

    completed javascript- timers exercise

---
+++ javascript-timers/interval.js
@@ -0,0 +1,14 @@
+var countdownMessage = document.querySelector('.countdown-display');
+
+var count = 4;
+
+function countdown() {
+  if (count > 0) {
+    countdownMessage.textContent = count--;
+  } else {
+    clearInterval(countdown);
+    countdownMessage.textContent = '~Earth Beeeelooowww Us~';
+  }
+}
+
+setInterval(countdown, 1000);
