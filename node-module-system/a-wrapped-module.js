91f7c8b15a29ee9ed30a334ad4bdac350dbc88d8
Kevin Liang <kevinliang415@yahoo.com>
2022-06-29 00:47:24 +0000

    completed node-module-system exercise

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('Type of:', typeof exports);
+console.log(exports);
+
+console.log('Type of:', typeof require);
+console.log(require);
+
+console.log('Type of:', typeof module);
+console.log(module);
+
+console.log('Type of:', typeof __filename);
+console.log(__filename);
+
+console.log('Type of:', typeof _dirname);
+console.log(__dirname);
