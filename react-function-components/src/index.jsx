ac586e2c6c59434c9347b0f48d4d8b378f6eda2d
Kevin Liang <kevinliang415@yahoo.com>
2022-07-13 23:07:21 +0000

    updated

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-const button = React.createElement(
-  'button',
-  [null],
-  ['Click Me!']
-);
-
-/* eslint-disable */
-console.log(button);
+function CustomButton() {
+  return (
+    <button>Click Me!</button>
+  );
+}
 
 const container = document.querySelector('#root');
 
 const root = ReactDOM.createRoot(container);
 
-root.render(button);
+root.render(<CustomButton />);
