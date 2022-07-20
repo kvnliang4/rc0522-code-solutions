b7f4bbaf8df98e3ac249c24d78dfa3a179a3bc16
Kevin Liang <kevinliang415@yahoo.com>
2022-07-20 18:22:29 +0000

    Realized i forgot to convert the 4XX requests

--- express-error-handling/server/index.js
+++ express-error-handling/server/index.js
@@ -36,17 +36,13 @@ app.post('/api/grades', (req, res, next) => {
   const { name, course } = req.body;
   const score = Number(req.body.score);
   if (!Number.isInteger(score) || score < 0 || score > 100) {
-    res.status(400).json({
-      error: 'score must be an integer between 0 and 100'
-    });
-    return;
+    throw new ClientError(400, 'score must be an integer between 0 and 100');
   }
+
   if (!name || !course) {
-    res.status(400).json({
-      error: 'name, course, and score are required fields'
-    });
-    return;
+    throw new ClientError(400, 'name, course, and score are required fields');
   }
+
   const sql = `
     insert into "grades" ("name", "course", "score")
     values ($1, $2, $3)
@@ -65,12 +61,10 @@ app.post('/api/grades', (req, res, next) => {
 
 app.get('/api/grades/:gradeId', (req, res, next) => {
   const gradeId = Number(req.params.gradeId);
-  if (!Number.isInteger(gradeId) || gradeId < 1) {
-    res.status(400).json({
-      error: 'grade must be a positive integer'
-    });
-    return;
+  if (!Number.isInteger(gradeId) || gradeId < 0) {
+    throw new ClientError(400, 'gradeId must be a positive integer');
   }
+
   const sql = `
     select *
       from "grades"
@@ -81,9 +75,7 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
     .then(result => {
       const [grade] = result.rows;
       if (!grade) {
-        res.status(404).json({
-          error: `cannot find grade with gradeId ${gradeId}`
-        });
+        throw new ClientError(404, `cannot find grade with gradeId ${gradeId}`);
       } else {
         res.json(grade);
       }
@@ -95,25 +87,16 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
 
 app.put('/api/grades/:gradeId', (req, res, next) => {
   const gradeId = Number(req.params.gradeId);
-  if (!Number.isInteger(gradeId) || gradeId < 1) {
-    res.status(400).json({
-      error: 'grade must be a positive integer'
-    });
-    return;
+  if (!Number.isInteger(gradeId) || gradeId < 0) {
+    throw new ClientError(400, 'gradeId must be a positive integer');
   }
   const { name, course } = req.body;
   const score = Number(req.body.score);
   if (!Number.isInteger(score) || score < 0 || score > 100) {
-    res.status(400).json({
-      error: 'score must be an integer between 0 and 100'
-    });
-    return;
+    throw new ClientError(400, 'score must be an integer between 0 and 100');
   }
   if (!name || !course) {
-    res.status(400).json({
-      error: 'name, course, and score are required fields'
-    });
-    return;
+    throw new ClientError(400, 'name, course, and score are required fields');
   }
   const sql = `
     update "grades"
@@ -128,9 +111,7 @@ app.put('/api/grades/:gradeId', (req, res, next) => {
     .then(result => {
       const [updatedGrade] = result.rows;
       if (!updatedGrade) {
-        res.status(404).json({
-          error: `cannot find grade with gradeId ${gradeId}`
-        });
+        throw new ClientError(404, `cannot find grade with gradeId ${gradeId}`);
       } else {
         res.json(updatedGrade);
       }
@@ -143,10 +124,7 @@ app.put('/api/grades/:gradeId', (req, res, next) => {
 app.delete('/api/grades/:gradeId', (req, res, next) => {
   const gradeId = Number(req.params.gradeId);
   if (!Number.isInteger(gradeId) || gradeId < 1) {
-    res.status(400).json({
-      error: 'grade must be a positive integer'
-    });
-    return;
+    throw new ClientError(400, 'grade must be a positive integer');
   }
   const sql = `
     delete from "grades"
@@ -158,9 +136,7 @@ app.delete('/api/grades/:gradeId', (req, res, next) => {
     .then(result => {
       const [deletedGrade] = result.rows;
       if (!deletedGrade) {
-        res.status(404).json({
-          error: `cannot find grade with gradeId ${gradeId}`
-        });
+        throw new ClientError(404, `cannot find grade with gradeId ${gradeId}`);
       } else {
         res.sendStatus(204);
       }
