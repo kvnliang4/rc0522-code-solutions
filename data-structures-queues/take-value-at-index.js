af6f51abfaafca187cea9c7ec2b94e9c45015a5c
Kevin Liang <kevinliang415@yahoo.com>
2022-08-10 19:46:24 +0000

    updated

--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,6 +1,9 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
+  if (queue.peek() === undefined) {
+    return queue.peek();
+  }
   for (let i = 0; i < index; i++) {
     queue.enqueue(queue.peek());
     queue.dequeue();
